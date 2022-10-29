0e3432d8fad7092752334e262df5e42f1a1bb3c6
Aman Raj <amanpie9@gmail.com>
2022-10-29 12:14:32 +0530

    seperate unit tests in different folders

---
+++ src/components/Client.test.js
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react";
+import { useRef } from "react";
+import App from "../App";
+import Client from "./Client";
+import Editor from "./Editor";
+
+
+describe("Client Details Rendering works as expected", () => {
+  test("Client Name renders as expected", () => {
+    const name = 'Test';
+    render(<Client userName={name}/>);
+    const client = screen.getByText(name)
+    expect(client).toBeInTheDocument();
+  });
+});
--- src/pages/HomeInetaction.test.js
+++ src/pages/HomeInetaction.test.js
@@ -1,22 +1,9 @@
 import { fireEvent, render, screen } from "@testing-library/react";
+import App from "../App";
 
-import App from "./App";
-
-describe("Test app renders properly", () => {
-  test("render texts in document", () => {
-    render(<App />);
-    const inviteText = screen.getByText(/Don't have an invitation code?/i);
-    expect(inviteText).toBeInTheDocument();
-
-    const createRoomText = screen.getByText(/Create a new room/i);
-    expect(createRoomText).toBeInTheDocument();
-
-    const joinRoomButton = screen.getByRole("button", { name: /Join/i });
-    expect(joinRoomButton).toBeInTheDocument();
-  });
-
+describe("Buttons works as expected", () => {
   test("create a new room works", () => {
-    render(<App />);
+    render(<App/>);
 
     const createRoomButton = screen.getByRole("button", {
       name: /Create a new room/,
@@ -41,12 +28,5 @@ describe("Test app renders properly", () => {
 
     const errorMessage = screen.getByText(/Please enter room id and user name/);
     expect(errorMessage).toBeInTheDocument();
-  });
-
-  test("footer rendered", () => {
-    render(<App />);
-
-    const amanName = screen.getByText(/Aman/);
-    expect(amanName).toBeInTheDocument();
   });
 });
