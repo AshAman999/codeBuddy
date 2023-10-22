ecdefee7f54fae706c980482bdd4b1cadc115212
Restyled.io <commits@restyled.io>
2023-10-22 09:13:57 +0000

    Restyled by clang-format

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 
-import { Toaster } from "react-hot-toast";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {Toaster} from "react-hot-toast";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 import LandingPage from "./pages/LandingPage/LandingPage";
 import NamePrompt from "./pages/NamePrompt/NamePrompt";
@@ -15,9 +15,11 @@ function App() {
       </div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<LandingPage />} />
+          <Route path="/" element={
+    <LandingPage />} />
           <Route path="/x" element={<LandingPage />} />
-          <Route path="/y" element={<NamePrompt />} />
+          <Route path="/y" element={
+    <NamePrompt />} />
           <Route path="/editorHome" element={<UserNameCheck />} />
         </Routes>
       </BrowserRouter>
