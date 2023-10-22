3eb94c9fb3bc1978400473a2a3656151b9f97a1b
Aman Raj <amanpie9@gmail.com>
2023-10-22 13:24:09 +0530

    client tests added

--- src/components/Client.js
+++ src/components/Client.js
@@ -2,22 +2,14 @@ import React from "react";
 import Avatar from "react-avatar";
 import styles from "./Client.module.css";
 
-// Function to shorten the name to a maximum of 4 characters
-const shortenName = (name) => {
-  if (name.length > 4) {
-    return name.slice(0, 4) + "...";
-  }
-  return name;
-};
-
-// Component to display a single client with its name and avatar
 export const Client = ({ userName }) => {
-  const shortName = shortenName(userName);
-
   return (
-    <div className={styles.client}>
-      <Avatar name={userName} size={"25"} round={"20px"} />
-      {/* <div className={styles.userName}>{shortName}</div> */}
+    <div className={styles.client} data-testid="client-component">
+      <Avatar
+        name={userName}
+        size={"25"}
+        round={"20px"}
+      />
     </div>
   );
 };
